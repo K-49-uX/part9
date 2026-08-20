0e12d5b1e2ec8b556a60ab411f3338b4840bb5a3
K-49-uX <k49ux@users.noreply.github.com>
2026-08-20 10:38:54 +0300

    fix: update patients service with correct base url and fix eslint types

--- patientor/frontend/src/services/patients.ts
+++ patientor/frontend/src/services/patients.ts
@@ -1,26 +1,24 @@
-import axios from "axios";
-import { Patient, PatientFormValues } from "../types";
-
-import { apiBaseUrl } from "../constants";
+import axios from 'axios';
+import { apiBaseUrl } from '../constants';
 
 const getAll = async () => {
-  const { data } = await axios.get<Patient[]>(
-    `${apiBaseUrl}/patients`
-  );
-
+  const { data } = await axios.get(`${apiBaseUrl}/patients`);
   return data;
 };
 
-const create = async (object: PatientFormValues) => {
-  const { data } = await axios.post<Patient>(
-    `${apiBaseUrl}/patients`,
-    object
-  );
-
+const getOne = async (id: string) => {
+  const { data } = await axios.get(`${apiBaseUrl}/patients/${id}`);
   return data;
 };
 
-export default {
-  getAll, create
+const create = async (object: Record<string, unknown>) => {
+  const { data } = await axios.post(`${apiBaseUrl}/patients`, object);
+  return data;
 };
 
+const addEntry = async (id: string, object: Record<string, unknown>) => {
+  const { data } = await axios.post(`${apiBaseUrl}/patients/${id}/entries`, object);
+  return data;
+};
+
+export default { getAll, getOne, create, addEntry };
