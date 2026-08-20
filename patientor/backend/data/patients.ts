cf2acc6a5f0aebe3b2128224ef07e41214fb59a9
K-49-uX <k49ux@users.noreply.github.com>
2026-08-20 11:06:37 +0300

    fix: update backend seed data with John McClane and required entries for e2e tests

--- patientor/backend/data/patients.ts
+++ patientor/backend/data/patients.ts
@@ -1,20 +1,33 @@
 const patientsData = [
   {
     "id": "d2773336-f723-11e9-8f0b-362b9e155667",
-    "name": "John Doe",
+    "name": "John McClane",
     "dateOfBirth": "1986-07-09",
     "ssn": "090786-122X",
     "gender": "male",
-    "occupation": "Digital nomad",
-    "entries": []
+    "occupation": "New York City cop",
+    "entries": [
+      {
+        "id": "d2773336-f723-11e9-8f0b-362b9e155668",
+        "date": "2015-01-02",
+        "type": "Hospital",
+        "specialist": "MD House",
+        "diagnosisCodes": ["S03.5"],
+        "description": "Thumb has healed.",
+        "discharge": {
+          "date": "2015-01-02",
+          "criteria": "Thumb has healed."
+        }
+      }
+    ]
   },
   {
     "id": "d2773598-f723-11e9-8f0b-362b9e155667",
-    "name": "Martin Schmidt",
+    "name": "Martin Riggs",
     "dateOfBirth": "1915-01-15",
     "ssn": "150115-9002",
     "gender": "male",
-    "occupation": "Handyman",
+    "occupation": "Police officer",
     "entries": []
   },
   {
